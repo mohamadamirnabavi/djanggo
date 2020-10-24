b9cb26912910a6911db25e36eaa75a780973e8a4
amir <mohamadamirnabavi@gmail.com>
2020-10-24 23:53:52 +0330

    import media to project

--- articles/views.py
+++ articles/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render,HttpResponse
 from . import models
 
 # Create your views here.
-def articles_list(request):
-    articles= models.Article.objects.all().order_by('date')
+def articles_list(request):#قبل از دیت اگر منفی بزاریم برعکس میشه ترتیب
+    articles= models.Article.objects.all().order_by('-date')
 
     args = {'articles':articles}
     return render(request, 'articles/articleslist.html',args )
 
 
 def article_detail(request, slug):
-    return HttpResponse(slug)
+    # return HttpResponse(slug)
+    article = models.Article.objects.get(slug=slug)
+
+    return render(request,'articles/articlesdetail.html',{'article':article})
