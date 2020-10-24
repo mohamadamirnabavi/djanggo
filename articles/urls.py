6f5914057ad8a1f5df9e664ec9d10c9d08fb5e1e
amir <mohamadamirnabavi@gmail.com>
2020-10-24 20:18:46 +0330

    dynamic urls

--- articles/urls.py
+++ articles/urls.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+
+app_name="articles"
+
 urlpatterns = [
-    path('' , views.articles_list),
+    path('' , views.articles_list, name= "list"),
+    path('<slug>', views.article_detail, name = "detail"),
 ]
