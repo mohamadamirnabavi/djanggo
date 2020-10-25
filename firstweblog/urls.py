7157aa4d50dbaa90bf0c57207e98159e1d682b49
amir <mohamadamirnabavi@gmail.com>
2020-10-25 23:17:14 +0330

    vs cod

--- firstweblog/urls.py
+++ firstweblog/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('about/', views.about),
     path('' , views.home),
     path('articles/', include('articles.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
