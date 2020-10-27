6bb78a3fd37fc58230898dc2829ff39c9578bf21
amir <mohamadamirnabavi@gmail.com>
2020-10-27 20:15:00 +0330

    login and logout

--- articles/urls.py
+++ articles/urls.py
@@ -4,9 +4,10 @@ from django.urls import path, include
 from . import views
 
 
-app_name="articles"
+app_name = "articles"
 
 urlpatterns = [
-    path('' , views.articles_list, name= "list"),
-    path('<slug>', views.article_detail, name = "detail"),
+    path('', views.articles_list, name="list"),
+    path('create', views.create_articles, name="create"),
+    path('<slug>', views.article_detail, name="detail"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,16 +1,24 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 from . import models
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-def articles_list(request):#قبل از دیت اگر منفی بزاریم برعکس میشه ترتیب
-    articles= models.Article.objects.all().order_by('-date')
 
-    args = {'articles':articles}
-    return render(request, 'articles/articleslist.html',args )
+
+def articles_list(request):  # قبل از دیت اگر منفی بزاریم برعکس میشه ترتیب
+    articles = models.Article.objects.all().order_by('-date')
+
+    args = {'articles': articles}
+    return render(request, 'articles/articleslist.html', args)
 
 
 def article_detail(request, slug):
     # return HttpResponse(slug)
     article = models.Article.objects.get(slug=slug)
 
-    return render(request,'articles/articlesdetail.html',{'article':article})
+    return render(request, 'articles/articlesdetail.html', {'article': article})
+
+
+@login_required(login_url="/accounts/login")
+def create_articles(request):
+    return render(request, 'articles/creat_articles.html')
